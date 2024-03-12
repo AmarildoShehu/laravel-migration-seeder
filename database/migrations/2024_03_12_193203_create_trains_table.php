38fbb12f78975ddbf242657b73e5ecf2c346a4b7
Amarildo <you@example.com>
2024-03-12 20:37:17 +0100

    created migration

---
+++ database/migrations/2024_03_12_193203_create_trains_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda');
+            $table->string('stazione_partenza');
+            $table->string('stazione_arrivo');
+            $table->time('orario_partenza');
+            $table->time('orario_arrivo');
+            $table->string('codice_treno');
+            $table->integer('numero_carrozze');
+            $table->boolean('in_orario')->default(true);
+            $table->boolean('cancellato')->default(false);
+
+            // Colonnes aggiuntive
+            $table->text('note')->nullable();
+            $table->decimal('prezzo', 8, 2)->nullable();
+            $table->date('data_partenza')->nullable();
+            $table->date('data_arrivo')->nullable();
+            $table->integer('passeggeri_massimi')->nullable();
+
+            // Aggiungi altre colonne se necessario
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
+
